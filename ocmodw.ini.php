d8cc2b7b10e8d3cc4aaa2cc0de38c47d07fe0f86
akaunderr <akaunderr@gmail.com>
2024-01-22 14:04:09 +0200

    updated module wrapper

---
+++ ocmodw.ini.php
@@ -0,0 +1,26 @@
+<?php
+
+define('MODVERS', '1.1.1');
+define('MODYEAR', '2023');
+
+define('AUTHOR', 'Andrii Burkatskyi');
+define('EMAIL', 'ocmod.space@gmail.com');
+
+define('MODLINK', 'https://www.ocmod.space/ocmod-estimated-date');
+define('OCMLINK', 'https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=45684');
+define('GITLINK', 'https://github.com/ocmod-space/ocmod-estimated-date');
+define('XMLLINK', 'https://www.opencart.com/index.php?route=marketplace/extension&amp;filter_member=ocmod.space');
+
+define('LICLINK', 'https://raw.githubusercontent.com/ocmod-space/ocmod-estimated-date/main/EULA.txt');
+
+///
+
+define('FCLDIR', '_fcl');
+define('FCLIGNORE', '.fclignore'); // ignorelist for fcl
+define('SRCDIR', 'src');
+define('ZIPDIR', 'zip');
+define('ZIPEXT', '.ocmod.zip');
+
+define('MDIR', 'module'); // dir with module
+define('ADIR', 'addons'); // dir with addons
+
